173a1bb07a3c85584222b835163cdbd26e3d4711
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2019-11-06 20:57:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -488,12 +488,12 @@ client.on('message', message => {
 client.on('message', message => {//new msg event
 if(!message.channel.guild) return;
   if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
-	  let role = message.guild.roles.find('name', 'Vrr')
+	  let role = message.guild.roles.find('name', 'Special Member')
     if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
   //start of create role 
   if(!role){
     rainbow =  message.guild.createRole({
-   name: "Vrr",//the role will create name
+   name: "Special Member",//the role will create name
    color: "#000000",//the default color
    permissions:[]//the permissions
  //end of create role
@@ -506,7 +506,7 @@ message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the s
 client.on('ready', () => {//new ready event
   setInterval(function(){
       client.guilds.forEach(g => {
-                  var role = g.roles.find('name', 'Vrr');//rainbow role name
+                  var role = g.roles.find('name', 'Special Member');//rainbow role name
                   if (role) {
                       role.edit({color : "RANDOM"});
                   };
